6e539665b5d92188d5af366d8dcbb81d996603ab
Petter Lindgren <petlindg@student.chalmers.se>
2024-01-14 15:20:48 +0100

    working on bot

--- src/bot.c
+++ src/bot.c
@@ -8,7 +8,7 @@
 #include "move.h"
 
 
-int evalPiece(Piece piece) {
+int evalPiece(Piece piece, Color color) {
     int val;
     switch(piece.pieceType) {
         case PAWN:
@@ -29,42 +29,66 @@ int evalPiece(Piece piece) {
         default:
             return 0;
     }
-    if(piece.color==BLACK) {
-        val*=-1;
+    if(piece.color != color) {
+        val *= -1;
     }
     return val;
 }
 
-//positive number favor white, negative favor black
+// evaluates gain in favor of color {color}
 int evalBoard(Piece board[8][8], Color color) {
     Move* moves = malloc(10*sizeof(Move));
     if(getMoves(board, color, &moves) == 0) {
-        if(color==WHITE) {
-            return -10000;
-        }
-        return 10000;
+        return -10000;
     }
     int val = 0;
     for(int i=0; i<8; i++) {
         for(int j=0; j<8; j++) {
-            val+=evalPiece(board[i][j]);
+            val+=evalPiece(board[i][j], color);
         }
     }
     return val;
 }
 
-/*
+
 // find best move brute force depth first
-Move findBestMove(Piece board[8][8], Color playColor, Color moveColor, Move* moves, int movesSize, int depth, Move* moveSeq) {
-    if(depth<=0) return;
+// bool color 0 white 1 black
+Move findBestMove(Piece board[8][8],
+                  Color playColor,
+                  Color otherColor,
+                  Move* moves,
+                  int movesSize,
+                  int depthCur,
+                  int depthMax,
+                  int* evalBest,
+                  int* evalCurr,
+                  Move* pathBest,
+                  Move* pathCurr) {
+    if(depthCur<=0) {
+        *evalCurr = evalBoard(board, playColor);
+        if(*evalCurr>*evalBest) {
+            *evalBest=*evalCurr;
+            for(int i = 0; i<depthMax; i++) {
+                pathBest[i]=pathCurr[i];
+            }
+        }
+        return;
+    }
     Piece boardClone[8][8];
     for(int i=0; i<movesSize; i++) {
         copyBoard(board, boardClone);
-        movePiece(boardClone, moves[i])
-        findBestMove
+        movePiece(boardClone, moves[i]);
+        Move* movesOther = malloc(10*sizeof(Move));
+        int movesSizeOther = getMoves(boardClone, otherColor, movesOther);
+        if(movesSizeOther==0) {
+            *evalBest==10000;
+            return pathCurr[0];
+        }
+        findBestMove(boardClone, otherColor, playColor, movesOther, movesSizeOther, depth-1);
+
+
 
         
     }
 
 }
-*/
